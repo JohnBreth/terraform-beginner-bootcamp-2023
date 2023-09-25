9a70fdf1d94b20c15caf982094510c2de9114349
John Breth <jbreth1@gmail.com>
2023-09-25 21:46:18 +0000

    #25 refactor S3 bucket into new Terrahouse AWS module

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
 
-# Bucket naming rules
-# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-  tags = {
-    UserUuid = var.user_uuid
-  }
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  #cloud {
+  #  organization = "ExamPro"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+
+}
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
