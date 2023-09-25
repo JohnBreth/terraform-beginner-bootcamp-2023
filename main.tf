48c43571dc42483cc73dbfa0537c8eb5d37d1c7f
John Breth <jbreth1@gmail.com>
2023-09-25 19:48:25 +0000

    #23 removed random and set variable for S3 bucket.

--- main.tf
+++ main.tf
@@ -1,17 +1,9 @@
 
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "bucket_name" {
-  length = 32
-  special = false
-  lower = true
-  upper = false
-}
 # Bucket naming rules
 # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
-
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
   tags = {
     UserUuid = var.user_uuid
   }
