e94f6712c80df04b9af542364e5de97091cc25cc
John Breth <jbreth1@gmail.com>
2023-10-05 14:49:10 +0000

    #41 wip implemeting Terraform provider block

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+
+required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #backend "remote" {
   #  hostname = "app.terraform.io"
   #  organization = "ExamPro"
@@ -16,12 +23,18 @@ terraform {
 
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  assets_path=var.assets_path
-  content_version = var.content_version
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   assets_path=var.assets_path
+#   content_version = var.content_version
+# }
