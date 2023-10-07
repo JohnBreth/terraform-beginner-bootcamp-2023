259f7724547f01380fe3161af9a58f03488995ef
John Breth <jbreth1@gmail.com>
2023-10-07 22:30:22 +0000

    #46 Tested integration of terra home with terratowns server

--- main.tf
+++ main.tf
@@ -24,20 +24,20 @@ required_providers {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.user_uuid
+  token = var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   assets_path=var.assets_path
-#   content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
 resource "terratowns_home" "home" {
   name = "Terraform For Beginners Bootcamp Infographics"
@@ -45,8 +45,8 @@ resource "terratowns_home" "home" {
 Here are some infographics that helped me understand some of the concepts 
 related to the topics covered in this bootcamp
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "cooker-cove"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "blahblah.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
